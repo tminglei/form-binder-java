4996e204f5cc1b33c67c2819420bd9e1c604529d
Minglei Tu <tmlneu@gmail.com>
2015-07-07 21:30:03 +0800

    some more java docs for BindObject

--- src/main/java/com/github/tminglei/bind/BindObject.java
+++ src/main/java/com/github/tminglei/bind/BindObject.java
@@ -20,24 +20,51 @@ public class BindObject implements Iterable<Map.Entry<String, Object>> {
         this.data = data;
     }
 
+    /**
+     * @param <T> expected type
+     * @return the errors optional
+     */
     public <T> Optional<T> errors() {
         return Optional.ofNullable((T) errors);
     }
 
+    /**
+     * implementation for interface Iterable
+     * @return the field iterator
+     */
     @Override
     public Iterator<Map.Entry<String, Object>> iterator() {
         return data.entrySet().iterator();
     }
+
+    /**
+     * @return field names
+     */
     public Set<String> names() {
         return data.keySet();
     }
 
+    /**
+     * @param name field name
+     * @return field bind object
+     */
     public BindObject node(String name) {
         return (BindObject) data.get(name);
     }
+
+    /**
+     * @param <T> expected type
+     * @return the final result value
+     */
     public <T> T get() {
         return (T) data.get(DEFAULT_KEY);
     }
+
+    /**
+     * @param name field name
+     * @param <T> expected type
+     * @return field value
+     */
     public <T> T get(String name) {
         return (T) data.get(name);
     }
